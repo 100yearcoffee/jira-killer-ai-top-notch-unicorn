5169108ab66e99b87f1a31dc19bf8a3a2ee83574
100yearscoffee <victorija45@gmail.com>
2026-07-03 13:46:02 +0200

    feat: local docker, docker-compose setup

--- go-api/internal/db/db.go
+++ go-api/internal/db/db.go
@@ -2,21 +2,30 @@ package db
 
 import (
 	"context"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func Connect(ctx context.Context, databaseURL string) (*pgxpool.Pool, error) {
-	pool, err := pgxpool.New(ctx, databaseURL)
-	if err != nil {
-		return nil, err
-	}
+	var lastErr error
 
-	err = pool.Ping(ctx)
-	if err != nil {
+	for range 10 {
+		pool, err := pgxpool.New(ctx, databaseURL)
+		if err != nil {
+			lastErr = err
+			time.Sleep(time.Second)
+			continue
+		}
+
+		err = pool.Ping(ctx)
+		if err == nil {
+			return pool, nil
+		}
+
+		lastErr = err
 		pool.Close()
-		return nil, err
+		time.Sleep(time.Second)
 	}
-
-	return pool, nil
+	return nil, lastErr
 }
